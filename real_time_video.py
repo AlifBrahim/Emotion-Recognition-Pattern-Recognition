2751d14ecb00db756ecbc0f76ca89c639a7d3d08
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-02-11 00:42:19 +0800

    Fix camera

--- real_time_video.py
+++ real_time_video.py
@@ -5,27 +5,49 @@ import cv2
 from keras.models import load_model
 import numpy as np
 
-# parameters for loading data and images
 detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
 emotion_model_path = 'models/_mini_XCEPTION.102-0.66.hdf5'
 
-# hyper-parameters for bounding boxes shape
-# loading models
 face_detection = cv2.CascadeClassifier(detection_model_path)
 emotion_classifier = load_model(emotion_model_path, compile=False)
 EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised", "neutral"]
 
 app = Flask(__name__)
 
-def gen():
-    camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        frame = camera.read()[1]
+        frame = imutils.resize(frame,width=300)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+        frameClone = frame.copy()
+        if len(faces) > 0:
+            faces = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+            (fX, fY, fW, fH) = faces
+            roi = gray[fY:fY + fH, fX:fX + fW]
+            roi = cv2.resize(roi, (64, 64))
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            preds = emotion_classifier.predict(roi)[0]
+            emotion_probability = np.max(preds)
+            label = EMOTIONS[preds.argmax()]
+        else: continue
+        cv2.putText(frameClone, label, (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
+        ret, jpeg = cv2.imencode('.jpg', frameClone)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def gen_canvas():
     while True:
         frame = camera.read()[1]
         frame = imutils.resize(frame,width=300)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
         canvas = np.zeros((250, 300, 3), dtype="uint8")
-        frameClone = frame.copy()
         if len(faces) > 0:
             faces = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
             (fX, fY, fW, fH) = faces
@@ -43,15 +65,17 @@ def gen():
             w = int(prob * 300)
             cv2.rectangle(canvas, (7, (i * 35) + 5), (w, (i * 35) + 35), (0, 0, 255), -1)
             cv2.putText(canvas, text, (10, (i * 35) + 23), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
-            cv2.putText(frameClone, label, (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-            cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
-        ret, jpeg = cv2.imencode('.jpg', frameClone)
-        frame = jpeg.tobytes()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        ret, jpeg = cv2.imencode('.jpg', canvas)
+        canvas = jpeg.tobytes()
+        yield (b'--canvas\r\n' b'Content-Type: image/jpeg\r\n\r\n' + canvas + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/canvas_feed')
+def canvas_feed():
+    return Response(gen_canvas(), mimetype='multipart/x-mixed-replace; boundary=canvas')
 
 @app.route('/')
 def index():
