02597404281f72e9adb217b787c6c26fd0cded4f
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-02-11 03:07:18 +0800

    Fix val_acc

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -51,7 +51,7 @@ early_stop = EarlyStopping('val_loss', patience=patience)
 reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
                                   patience=int(patience/4), verbose=1)
 trained_models_path = base_path + '_mini_XCEPTION'
-model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+model_names = trained_models_path + '.{epoch:02d}-{val_accuracy:.2f}.hdf5'
 model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
                                                     save_best_only=True)
 callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
@@ -61,11 +61,11 @@ faces, emotions = load_fer2013()
 faces = preprocess_input(faces)
 num_samples, num_classes = emotions.shape
 xtrain, xtest,ytrain,ytest = train_test_split(faces, emotions,test_size=0.2,shuffle=True)
-model.fit_generator(data_generator.flow(xtrain, ytrain,
-                                            batch_size),
-                        steps_per_epoch=len(xtrain) / batch_size,
-                        epochs=num_epochs, verbose=1, callbacks=callbacks,
-                        validation_data=(xtest,ytest))
+model.fit(data_generator.flow(xtrain, ytrain, batch_size),
+          steps_per_epoch=len(xtrain) / batch_size,
+          epochs=num_epochs, verbose=1, callbacks=callbacks,
+          validation_data=(xtest,ytest))
+
 
 
 # SVM
